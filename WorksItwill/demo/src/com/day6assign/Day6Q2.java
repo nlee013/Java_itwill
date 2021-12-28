17ceeb45cda5b76b50dcddb7f1c71b2f21f13d26
nlee013 <83215027+nlee013@users.noreply.github.com>
2021-12-28 18:21:21 +0900

    add day6 assign

---
+++ WorksItwill/demo/src/com/day6assign/Day6Q2.java
@@ -0,0 +1,17 @@
+package com.day6assign;
+
+import java.io.IOException;
+
+public class Day6Q2 {
+
+	public static void main(String[] args) throws IOException{
+
+		EvenOdd yo = new EvenOdd();
+		yo.input();
+		
+		String strr = yo.result();
+		yo.print(strr);
+		
+	}
+
+}
---
+++ WorksItwill/demo/src/com/day6assign/Day6Q3.java
@@ -0,0 +1,12 @@
+package com.day6assign;
+
+public class Day6Q3 {
+
+	public static void main(String[] args) {
+
+		Star st = new Star();
+		st.input();
+		
+		int aa = st.type();
+	}
+}
