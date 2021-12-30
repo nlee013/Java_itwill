a26a45776b4b50c7b5c6888f18bb171822b00fe7
nlee013 <83215027+nlee013@users.noreply.github.com>
2021-12-30 10:51:13 +0900

    update and practice fitness2

--- WorksItwill/demo/src/com/day7/Test34.java
+++ WorksItwill/demo/src/com/day7/Test34.java
@@ -5,6 +5,8 @@ package com.day7;
 //정석 : 하나의 file에는 하나의 class 만 들어감
 //종종 하나의 file에 두개의 class를 만든다.
 
+//method는 처음 만들 때 하나만 생성.
+
 //main절에 들어간 곳만 public을 설정해야 VM(Virtual Machine)이
 //main으로만 찾아가 인식하기 때문
 //class파일은 bin 폴더에 있고 class파일을 2개 만들면 bin폴더에도 2개 생성
