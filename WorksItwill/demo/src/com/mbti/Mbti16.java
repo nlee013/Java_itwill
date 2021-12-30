2006ba5aee226ece9e9b4b6bdf65178c7c70de18
nlee013 <83215027+nlee013@users.noreply.github.com>
2021-12-30 18:25:44 +0900

    last update today and added mbti practice

---
+++ WorksItwill/demo/src/com/mbti/Mbti16.java
@@ -0,0 +1,8 @@
+package com.mbti;
+
+public class Mbti16 {
+	
+	String name;
+	String[] Qs = new String[4];
+
+}
---
+++ WorksItwill/demo/src/com/mbti/MbtiMain.java
@@ -0,0 +1,11 @@
+package com.mbti;
+
+public class MbtiMain {
+	
+	public static void main(String[] args) {
+		
+		Mbti fi = new Mbti();
+		
+		
+	}
+}
